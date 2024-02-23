2da71d0008aaae489cb81a24f2f8c8316a5336eb
Arwa <144407619+Arwa119@users.noreply.github.com>
2024-02-23 08:43:13 +0500

    Pd_LAb_4

---
+++ Week4Pd/OceanNavi/Angle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocean_Navigation
+{
+    class Angle
+    {
+        public int degree;
+        public float minutes;
+        public char direction;
+    }
+}
